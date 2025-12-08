51a42b1b5e21c671dccc9da5eff6d3ffa9e1c0ec
aziz <alhum@ucdavis.edu>
2025-12-08 07:39:11 -0800

    Added new map, currently adding new building boundries

--- aggiemon/Scripts/health_bar.gd
+++ aggiemon/Scripts/health_bar.gd
@@ -14,16 +14,15 @@ func _on_health_changed(_new_hp: int = 0):
 	self.max_value = enemy.max_hp
 	self.value = enemy.curr_hp
 	
-	# Color-code health bar based on HP percentage
 	var hp_percent = (enemy.curr_hp / enemy.max_hp) * 100
 	var fill_style = self.get_theme_stylebox("fill") as StyleBoxFlat
 	if fill_style:
 		if hp_percent > 60:
-			fill_style.bg_color = Color(0, 1, 0, 1)  # Green
+			fill_style.bg_color = Color(0, 1, 0, 1) 
 		elif hp_percent > 30:
-			fill_style.bg_color = Color(1, 1, 0, 1)  # Yellow
+			fill_style.bg_color = Color(1, 1, 0, 1) 
 		else:
-			fill_style.bg_color = Color(1, 0, 0, 1)  # Red
+			fill_style.bg_color = Color(1, 0, 0, 1)  
 	
 	label.text = str(int(self.value)) + " / " + str(int(self.max_value))
 
@@ -31,15 +30,14 @@ func update_health_bar():
 	self.max_value = enemy.max_hp
 	self.value = enemy.curr_hp
 	
-	# Color-code health bar based on HP percentage
 	var hp_percent = (enemy.curr_hp / enemy.max_hp) * 100
 	var fill_style = self.get_theme_stylebox("fill") as StyleBoxFlat
 	if fill_style:
 		if hp_percent > 60:
-			fill_style.bg_color = Color(0, 1, 0, 1)  # Green
+			fill_style.bg_color = Color(0, 1, 0, 1)  
 		elif hp_percent > 30:
-			fill_style.bg_color = Color(1, 1, 0, 1)  # Yellow
+			fill_style.bg_color = Color(1, 1, 0, 1)  
 		else:
-			fill_style.bg_color = Color(1, 0, 0, 1)  # Red
+			fill_style.bg_color = Color(1, 0, 0, 1)  
 	
 	label.text = str(int(self.value)) + " / " + str(int(self.max_value))
