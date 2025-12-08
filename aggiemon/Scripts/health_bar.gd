7e8c4b20ad2462cd94a1e252200d6398b10e1095
aziz <alhum@ucdavis.edu>
2025-12-08 07:28:43 -0800

    Partial changes to battle UI, to be continued after i finish integrating the new map

--- aggiemon/Scripts/health_bar.gd
+++ aggiemon/Scripts/health_bar.gd
@@ -14,10 +14,32 @@ func _on_health_changed(_new_hp: int = 0):
 	self.max_value = enemy.max_hp
 	self.value = enemy.curr_hp
 	
+	# Color-code health bar based on HP percentage
+	var hp_percent = (enemy.curr_hp / enemy.max_hp) * 100
+	var fill_style = self.get_theme_stylebox("fill") as StyleBoxFlat
+	if fill_style:
+		if hp_percent > 60:
+			fill_style.bg_color = Color(0, 1, 0, 1)  # Green
+		elif hp_percent > 30:
+			fill_style.bg_color = Color(1, 1, 0, 1)  # Yellow
+		else:
+			fill_style.bg_color = Color(1, 0, 0, 1)  # Red
+	
 	label.text = str(int(self.value)) + " / " + str(int(self.max_value))
 
 func update_health_bar():
 	self.max_value = enemy.max_hp
 	self.value = enemy.curr_hp
+	
+	# Color-code health bar based on HP percentage
+	var hp_percent = (enemy.curr_hp / enemy.max_hp) * 100
+	var fill_style = self.get_theme_stylebox("fill") as StyleBoxFlat
+	if fill_style:
+		if hp_percent > 60:
+			fill_style.bg_color = Color(0, 1, 0, 1)  # Green
+		elif hp_percent > 30:
+			fill_style.bg_color = Color(1, 1, 0, 1)  # Yellow
+		else:
+			fill_style.bg_color = Color(1, 0, 0, 1)  # Red
 	
 	label.text = str(int(self.value)) + " / " + str(int(self.max_value))
