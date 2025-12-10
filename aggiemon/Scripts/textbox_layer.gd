0f0b5f6c1249575d7ab5ca60e2078f8b2249d422
Abraham Castellon <aycast@ucdavis.edu>
2025-12-10 00:51:26 -0800

    Added dialogue animations with sound effects

--- aggiemon/Scripts/textbox_layer.gd
+++ aggiemon/Scripts/textbox_layer.gd
@@ -4,8 +4,11 @@ extends CanvasLayer
 @onready var textbox_container = $Textbox
 @onready var label = $Textbox/Panel/MarginContainer/HBoxContainer/Label
 @onready var text_sfx: AudioStreamPlayer = $TextSFX
+@onready var tween = get_tree().create_tween()
+@onready var player : Player = $"../Player"
 
-var tween
+@export var num_visible_character_per_loop = 2
+@export var speed_of_character_read = 0.03
 
 func _ready() -> void:
 	hide_textbox()
@@ -19,11 +22,15 @@ func show_textbox():
 	
 func add_text(text):
 	label.text = text
+	label.visible_ratio = 0.0
 	show_textbox()
-	if text_sfx:
-		if text_sfx.playing:
-			text_sfx.stop()
-		text_sfx.play()
+	for i in label.text.length():
+		label.visible_characters = i + num_visible_character_per_loop
+		await get_tree().create_timer(speed_of_character_read).timeout
+		if text_sfx:
+			if text_sfx.playing:
+				text_sfx.stop()
+			text_sfx.play()
 	
 	# Calculate display time based on text length (roughly 0.05 seconds per character, minimum 4 seconds)
 	var display_time = max(4.0, text.length() * 0.05)
